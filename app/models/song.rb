2984397bfb944fef6a83f7082882c18f5cde6fe2
Brittany Javalera <brittjavalera@gmail.com>
2019-10-31 20:14:08 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,9 @@
 class Song < ApplicationRecord
     validates :title, presence: true, uniqueness: { scope: [:release_year, :artist_name]}
-    
+    validates :artist_name, presence: true
+    validates :release_year, presence: true, numericality: { less_than: Date.current.year }, if: :is_released?
+end
+
+def is_released?
+    self.released == true
 end
