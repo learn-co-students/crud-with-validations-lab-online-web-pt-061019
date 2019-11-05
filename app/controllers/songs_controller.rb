273977f28ed761e35f90a7ef0c091388131e9ecd
ZumbaIdol <david.a.foote@gmail.com>
2019-11-04 18:36:08 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,16 +6,25 @@ class SongsController < ApplicationController
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
-    @song = Song.new
+    @song = Song.new(song_params)
   end
 
   def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
+    @song = Song.find(params[:id])
   end
 
   def update
@@ -24,5 +33,9 @@ class SongsController < ApplicationController
   private
     def set_song
       @song = Song.find(params[:id])
+    end
+
+    def song_params
+      params.permit(:title, :released, :release_year, :artist_name, :genre)
     end
 end
